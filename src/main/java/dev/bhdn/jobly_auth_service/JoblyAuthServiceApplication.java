68460d4fe11fd1972fcbd8158348dea78070d803
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2025-10-03 20:10:21 +0200

    Added infrastructure

---
+++ src/main/java/dev/bhdn/jobly_auth_service/JoblyAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.bhdn.jobly_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblyAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoblyAuthServiceApplication.class, args);
+    }
+
+}
