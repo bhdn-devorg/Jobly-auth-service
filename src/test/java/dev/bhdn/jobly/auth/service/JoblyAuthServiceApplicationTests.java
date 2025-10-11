beb0dffc8b069be536cebdc4c2022906e5bc1ee6
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2025-10-11 14:52:37 +0200

    Disabled tests

--- src/test/java/dev/bhdn/jobly/auth/service/JoblyAuthServiceApplicationTests.java
+++ src/test/java/dev/bhdn/jobly/auth/service/JoblyAuthServiceApplicationTests.java
@@ -1,13 +1,12 @@
 package dev.bhdn.jobly.auth.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JoblyAuthServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
